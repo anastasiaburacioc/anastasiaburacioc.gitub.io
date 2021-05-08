94a0795b36d0cdafacfd01c5b5212fad413c0815
anastasiaburacioc <57499591+anastasiaburacioc@users.noreply.github.com>
2021-05-08 13:55:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,22 +4,16 @@ function alertt() {
 alertt();
 let circle = document.querySelector('.circle');
 let mainSound = document.querySelector('.main-sound');
-function playSound() {
-    mainSound.play();
-}
-
-
 let container = document.querySelector('.containner');
 let section = document.querySelector('.section');
 let mainText = document.querySelector('.main-text');
-
 
 function animateCircle() {
     document.querySelector('.circle_inner').remove();
     circle.classList.add('animate-circle');
     section.classList.remove('displayNone');
     mainText.classList.remove('displayNone');
-
+    mainSound.play();
 }
 circle.onclick = animateCircle;
 
